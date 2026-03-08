8f739c87a4f60da6d61870953a5766bd987c7aca
Vladimir Pasechnik <w.tiger.w@yandex.ru>
2026-03-08 21:24:36 +0300

    initial

--- eureka-client-2/src/main/java/ru/javabegin/micro/demo/eurekaclient2/kafka/UserKafkaProducer.java
+++ eureka-client-2/src/main/java/ru/javabegin/micro/demo/eurekaclient2/kafka/UserKafkaProducer.java
@@ -1,5 +1,6 @@
 package ru.javabegin.micro.demo.eurekaclient2.kafka;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -18,10 +19,18 @@ public class UserKafkaProducer {
         this.kafkaTemplate = kafkaTemplate;
     }
 
+    @CircuitBreaker(name = "kafkaProducer", fallbackMethod = "sendToKafkaFallback")
     public void sendOrderToKafka(UserNotificationDto userNotificationDto){
         kafkaTemplate.send("users", userNotificationDto.getUserId(), userNotificationDto);
         log.info("Order sent to kafka: id={}", userNotificationDto.getUserId());
 
+    }
+
+    // Fallback метод (обязательно должен быть в том же классе)
+    public void sendToKafkaFallback(UserNotificationDto userNotificationDto, Exception e) {
+        log.error("Failed to send to Kafka, using fallback: {}", userNotificationDto.getUserId(), e);
+        // Здесь можно просто логировать или сохранять в файл
+        System.err.println("Сообщение не отправлено в Kafka: " + userNotificationDto.getUserId());
     }
 
 }
